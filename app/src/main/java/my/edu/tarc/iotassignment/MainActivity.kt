524cc17b4a6a08ecd8b85355bbc59f9a7d77f916
CHAN-MH <chanmh-wp18@student.tarc.edu.my>
2020-11-22 10:24:00 +0800

    login validation change to without firebase authentication

--- app/src/main/java/my/edu/tarc/iotassignment/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/iotassignment/MainActivity.kt
@@ -7,29 +7,23 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 
 
 class MainActivity : AppCompatActivity() {
-    //Global variable
-    private lateinit var auth: FirebaseAuth
-    private lateinit var email1: TextView
-    private lateinit var password1: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //connect to teacher firebase
-        //val secondary = FirebaseDatabase.getInstance("https://bait2123-202010-03.firebaseio.com/")
+        //connect to personal firebase
+        val database2 = FirebaseDatabase.getInstance("https://solenoid-lock-f65e8.firebaseio.com/")
+        val loginRef: DatabaseReference = database2.getReference("UserProfile")
 
-        //connect to personal database
-        auth = FirebaseAuth.getInstance()
-
+        //Link UI to program
         val buttonLogin: Button = findViewById(R.id.buttonLogin)
-        email1 = findViewById(R.id.editTextEmail)
-        password1 = findViewById(R.id.editTextPassword)
+        var email1: TextView = findViewById(R.id.editTextEmail)
+        var password1: TextView = findViewById(R.id.editTextPassword)
 
         //start of login button
         buttonLogin.setOnClickListener() {
@@ -46,18 +40,26 @@ class MainActivity : AppCompatActivity() {
                 password1.error = "Password is required"
                 loginStatus = "false"
             }
-            if (loginStatus == "true") {
-                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success direct user to reservation page
-                        startActivity(Intent(this, DisplayRoom::class.java))
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(baseContext, "Login failed. Please try again", Toast.LENGTH_SHORT).show()
+            loginRef.child("User1").addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    var defaultemail = dataSnapshot.child("email").value
+                    var defaultpassword = dataSnapshot.child("password").value
+                    if (email == defaultemail && password == defaultpassword) {
+                        loginStatus = "true"
                     }
                 }
+                override fun onCancelled(error: DatabaseError) {
+                    // Actions when failed to read value
+                    Toast.makeText(baseContext, "Load Failed. Please try again", Toast.LENGTH_SHORT).show()
+                }
+            })
+            if (loginStatus == "true") {
+                // Sign in success direct user to reservation page
+                startActivity(Intent(this, DisplayRoom::class.java))
+            } else {
+                // If sign in fails, display a message to the user.
+                Toast.makeText(baseContext, "Login failed. Please try again", Toast.LENGTH_SHORT).show()
             }
         }//end of login button
-
-    }
-}
+    }//end of onCreate
+}//end of MainActivity class
